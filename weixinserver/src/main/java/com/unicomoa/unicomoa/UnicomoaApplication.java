8b4c3e7713297983707f254b1570d22db094b475
jiayongqiang <919506464@qq.com>
2018-12-14 13:10:09 +0800

    1

---
+++ weixinserver/src/main/java/com/unicomoa/unicomoa/UnicomoaApplication.java
@@ -0,0 +1,14 @@
+package com.unicomoa.unicomoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicomoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicomoaApplication.class, args);
+	}
+
+}
+
---
+++ weixinserver/src/test/java/com/unicomoa/unicomoa/UnicomoaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.unicomoa.unicomoa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnicomoaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
