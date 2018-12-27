42433554e5fe9a648d299644a6de3b8e4941e014
jiayongqiang <919506464@qq.com>
2018-12-27 15:37:58 +0800

    1

--- weixin/pages/my/index.js
+++ weixin/pages/my/index.js
@@ -1,101 +1,24 @@
-import { $wuxDialog } from '../../wux/index'
 const { PG, REQ } = require("../common/base_1.js")
 const CONFIG = require("../common/config_1.js")
-Page({
+PG({
   /**
    * 页面的初始数据
    */
   data: {
     dataType:''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-  logOut(e) {
-    this.custom()
-  },
-  custom() {
-    const alert = (content) => {
-      $wuxDialog('#wux-dialog--alert').alert({
-        resetOnClose: true,
-        title: '提示',
-        content: content
-      })
-    }
-    var that = this;
-    $wuxDialog().open({
-      resetOnClose: true,
-      title: '退出登录',
-      content: '确定要退出登录吗?',
-      buttons: [{
-        text: '确定',
-        type: 'primary',
-        onTap(e) {
-          wx.removeStorage({
-            key: 'userInfo',
-            success: function (res) {
-              console.log(res.data)
-            }
-          })
+  logout(e) {
+    wx.showModal({
+      title: '',
+      content: '确认退出登录?',
+      confirmColor:"#1296db",
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
         }
-      },
-      {
-        text: '取消',
-      },
-      ]
+      }
     })
   }
 })
