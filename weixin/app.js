85def98e4304038f165004bd7476012d7a104a28
海蓝透了心 <434557245@qq.com>
2019-01-03 09:53:36 +0800

    增加获取openId

--- weixin/app.js
+++ weixin/app.js
@@ -1,17 +1,52 @@
 //app.js
 App({
+  globalData: {
+    appid: '1wqas2342dasaqwe2323424ac23qwe',//appid需自己提供，此处的appid我随机编写
+    secret: 'e0dassdadef2424234209bwqqweqw123ccqwa',//secret需自己提供，此处的secret我随机编写
+  },
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    var that = this
+    var user = wx.getStorageSync('wxUser') || {};
+    var userInfo = wx.getStorageSync('wxUserInfo') || {};
+    if ((!user.openid || (user.expires_in || Date.now()) < (Date.now() + 600)) && (!userInfo.nickName)) {
+      wx.login({
+        success: function (res) {
+          if (res.code) {
+            wx.getUserInfo({
+              success: function (res) {
+                var objz = {}; 
+                objz.avatarUrl = res.userInfo.avatarUrl;
+                objz.nickName = res.userInfo.nickName;
+                console.log(objz);
+                wx.setStorageSync('wxUserInfo', objz);//存储userInfo
+              }
+            });
+            var d = that.globalData;//这里存储了appid、secret、token串  
+            var l = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + d.appid + '&secret=' + d.secret + '&js_code=' + res.code + '&grant_type=authorization_code';
+            wx.request({
+              url: l,
+              data: {},
+              method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+              // header: {}, // 设置请求的 header  
+              success: function (res) {
+                var obj = {};
+                obj.openid = res.data.openid;
+                obj.expires_in = Date.now() + res.data.expires_in;
+                console.log(obj);
+                wx.setStorageSync('wxUser', obj);//存储openid  
+              }
+            });
+          } else {
+            console.log('获取用户登录态失败！' + res.errMsg)
+          }
+        }
+      });
+    }
+  
     // 获取用户信息
     wx.getSetting({
       success: res => {
